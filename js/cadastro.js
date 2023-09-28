a1a969fdf523b24980ce8d7f9cf1723bdcf0660a
João Paulo Godinho <j_paulo_g@hotmail.com>
2023-09-28 20:05:30 -0300

    zzzz

--- js/cadastro.js
+++ js/cadastro.js
@@ -3,7 +3,7 @@ function gravar(email, senha1, senha2) {
     localStorage.setItem(email, senha1)
     alert('Salvo com sucesso.')
     const frm = document.getElementById('frm')
-    frm.setAttribute('action', 'https://jppg1992.github.io/projeto-e-comerce/')
+    frm.setAttribute('action', 'https://jppg1992.github.io/projeto-e-comerce')
   } else {
     alert('Senhas diferentes, cadastro não foi salvo.')
     throw Error('Senhas diferentes, cadastro não foi salvo.')
